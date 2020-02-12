2416e3d62beaf0769bf57cc23f49cb89b41db645
Daniel <daniel.avila@rottay.com>
2020-02-12 09:00:00 -0300

    Fix

--- src/routes/issues.js
+++ src/routes/issues.js
@@ -21,18 +21,16 @@ router.get('/issues', async (req, res) => {
 
 router.get('/issues/:user', async (req, res) => {
 
-  
-
   let issues_ = await getIssues();
   let users = await getUsers();
 
   // Fix : need an extra validation to filter errors by user
   errors.clear();
-  
+
   let issues = issues_.filter(i => i.opener === req.params.user);
   res.render('issues/all-issues', { issues, users, errors });
 
-});
+}); 
 
 
 module.exports = router;
